f1c1f2a1e28adb7be4460cf8b0f78deecd195f61
LeeTaDa <160697239+LeeTaDa@users.noreply.github.com>
2024-11-06 22:50:29 +0700

    SuaDE

--- src/main/java/com/example/demo/Controller/FileUploadController.java
+++ src/main/java/com/example/demo/Controller/FileUploadController.java
@@ -1,48 +1,48 @@
-package com.example.demo.Controller;
-
-import com.example.demo.service.CloudinaryService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-@RestController
-@CrossOrigin("*")
-@RequestMapping("/api/files")
-public class FileUploadController {
-
-    private static final Logger log = LoggerFactory.getLogger(FileUploadController.class);
-    private final CloudinaryService cloudinaryService;
-
-    public FileUploadController(CloudinaryService cloudinaryService) {
-        this.cloudinaryService = cloudinaryService;
-    }
-
-    @PostMapping("/upload/image")
-    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file,
-                                         @RequestParam("folder") String folderName) throws IOException {
-        log.info("Uploading image");
-        return ResponseEntity.ok(cloudinaryService.uploadFile(file, folderName));
-    }
-
-    @PostMapping("/upload/video")
-    public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file,
-                                         @RequestParam("folder") String folderName) throws IOException {
-        return ResponseEntity.ok(cloudinaryService.uploadVideo(file, folderName));
-    }
-
-    @PostMapping("/upload/audio")
-    public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile file,
-                                         @RequestParam("folder") String folderName) throws IOException {
-        return ResponseEntity.ok(cloudinaryService.uploadAudio(file, folderName));
-    }
-
-    @PostMapping("/upload/pdf")
-    public ResponseEntity<?> uploadPdf(@RequestParam("file") MultipartFile file,
-                                         @RequestParam("folder") String folderName) throws IOException {
-        return ResponseEntity.ok(cloudinaryService.uploadPdf(file, folderName));
-    }
-}
+//package com.example.demo.Controller;
+//
+//import com.example.demo.service.impl.CloudinaryService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//
+//@RestController
+//@CrossOrigin("*")
+//@RequestMapping("/api/files")
+//public class FileUploadController {
+//
+//    private static final Logger log = LoggerFactory.getLogger(FileUploadController.class);
+//    private final CloudinaryService cloudinaryService;
+//
+//    public FileUploadController(CloudinaryService cloudinaryService) {
+//        this.cloudinaryService = cloudinaryService;
+//    }
+//
+//    @PostMapping("/upload/image")
+//    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file,
+//                                         @RequestParam("folder") String folderName) throws IOException {
+//        log.info("Uploading image");
+//        return ResponseEntity.ok(cloudinaryService.uploadFile(file, folderName));
+//    }
+//
+//    @PostMapping("/upload/video")
+//    public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file,
+//                                         @RequestParam("folder") String folderName) throws IOException {
+//        return ResponseEntity.ok(cloudinaryService.uploadVideo(file, folderName));
+//    }
+//
+//    @PostMapping("/upload/audio")
+//    public ResponseEntity<?> uploadAudio(@RequestParam("file") MultipartFile file,
+//                                         @RequestParam("folder") String folderName) throws IOException {
+//        return ResponseEntity.ok(cloudinaryService.uploadAudio(file, folderName));
+//    }
+//
+//    @PostMapping("/upload/pdf")
+//    public ResponseEntity<?> uploadPdf(@RequestParam("file") MultipartFile file,
+//                                         @RequestParam("folder") String folderName) throws IOException {
+//        return ResponseEntity.ok(cloudinaryService.uploadPdf(file, folderName));
+//    }
+//}
