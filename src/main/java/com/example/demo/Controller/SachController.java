f627fdd387196816d879654c87abe22edb3d85a5
Long3112 <128303674+Long3112@users.noreply.github.com>
2024-12-01 13:45:48 +0700

    fix

--- src/main/java/com/example/demo/Controller/SachController.java
+++ src/main/java/com/example/demo/Controller/SachController.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @CrossOrigin("*")
 @RequestMapping("/sachs")
 public class SachController {
